3eaacc8bf5fe6d77cb5ed8a5006c398c8bbb4e9f
João Sena <joaosena0206@gmail.com>
2025-03-12 14:04:29 -0300

    feat: adiciona btn de continuar

--- results-summary/src/features/summary/Summary.jsx
+++ results-summary/src/features/summary/Summary.jsx
@@ -5,9 +5,11 @@ export default function Summary({ metricsData }) {
         <section className="py-5 px-8">
             <h1 className="mb-5 font-bold text-[#2F3649]">Summary</h1>
 
-            <Metrics
-                metricsData={metricsData}
-            />
+            <Metrics metricsData={metricsData} />
+
+            <button className="bg-[#303B59] text-white w-full py-4 rounded-full mt-10 font-bold">
+                Continue
+            </button>
         </section>
     );
 }
